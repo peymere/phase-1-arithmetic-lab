cdffc4cfb825cbb54b1accebc5f8f272a3bb98b1
Peyton Meredith <peymeredith@gmail.com>
2023-09-11 19:44:29 -0500

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+// console.log(multiply)
+
+const random = Math.floor(Math.random() * 100) + 1
+
+// console.log(random)
+
+let num3 = 44;
+let num4 = 10;
+const mod = num3 % num4;
+
+// console.log(mod)
+
+const max = Math.max(9, 4, 12, 20)
+
+console.log(max)
